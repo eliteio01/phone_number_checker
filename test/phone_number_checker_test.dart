1864610b80e7d2c521ff045bb7296a90e77e98e1
eliteio01 <etech.io02@gmail.com>
2025-04-27 16:43:53 +0100

    first commit

--- test/phone_number_checker_test.dart
+++ test/phone_number_checker_test.dart
@@ -4,29 +4,29 @@ import 'package:phone_number_checker/phone_number_checker.dart';
 void main() {
   group('NetworkChecker Tests', () {
     test('should detect MTN numbers', () {
-      expect(NetworkChecker.getNetwork('08031234567'), NetworkProvider.MTN);
-      expect(NetworkChecker.getNetwork('+2348031234567'), NetworkProvider.MTN);
+      expect(NetworkChecker.getNetwork('08031234567'), NetworkProvider.mtn);
+      expect(NetworkChecker.getNetwork('+2348031234567'), NetworkProvider.mtn);
     });
 
     test('should detect Airtel numbers', () {
-      expect(NetworkChecker.getNetwork('08021234567'), NetworkProvider.Airtel);
-      expect(NetworkChecker.getNetwork('+2348021234567'), NetworkProvider.Airtel);
+      expect(NetworkChecker.getNetwork('08021234567'), NetworkProvider.airtel);
+      expect(NetworkChecker.getNetwork('+2348021234567'), NetworkProvider.airtel);
     });
 
     test('should detect Glo numbers', () {
-      expect(NetworkChecker.getNetwork('08051234567'), NetworkProvider.Glo);
-      expect(NetworkChecker.getNetwork('+2348051234567'), NetworkProvider.Glo);
+      expect(NetworkChecker.getNetwork('08051234567'), NetworkProvider.glo);
+      expect(NetworkChecker.getNetwork('+2348051234567'), NetworkProvider.glo);
     });
 
     test('should detect 9mobile numbers', () {
-      expect(NetworkChecker.getNetwork('08091234567'), NetworkProvider.NineMobile);
-      expect(NetworkChecker.getNetwork('+2348091234567'), NetworkProvider.NineMobile);
+      expect(NetworkChecker.getNetwork('08091234567'), NetworkProvider.nineMobile);
+      expect(NetworkChecker.getNetwork('+2348091234567'), NetworkProvider.nineMobile);
     });
 
     test('should return Unknown for invalid numbers', () {
-      expect(NetworkChecker.getNetwork('08191234567'), NetworkProvider.Unknown); // Not defined
-      expect(NetworkChecker.getNetwork('12345'), NetworkProvider.Unknown);
-      expect(NetworkChecker.getNetwork(''), NetworkProvider.Unknown);
+      expect(NetworkChecker.getNetwork('08191234567'), NetworkProvider.unknown); // Not defined
+      expect(NetworkChecker.getNetwork('12345'), NetworkProvider.unknown);
+      expect(NetworkChecker.getNetwork(''), NetworkProvider.unknown);
     });
   });
 }
